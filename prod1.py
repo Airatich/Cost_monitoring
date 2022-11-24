5811f865a76e6ebeb84cadc54edd83b2c77a8476
Airatich <106591216+Airatich@users.noreply.github.com>
2022-11-24 09:32:17 +0300

    sec
    now the data for upload is stored in the excel file

--- prod1.py
+++ prod1.py
@@ -1,60 +1,74 @@
 # -*- coding: utf -*-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
 import bs4
 from selenium.webdriver.common.keys import Keys
 import re
 import pandas as pd
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-from options import user_agent, thickness_prod1
+from options import user_agent, thickness_prod1,path_to_project,cities_start
 from tqdm.auto import tqdm
 import time
+from datetime import datetime
+
+
 
 url = "http://23met.ru/"
 options = webdriver.ChromeOptions()
-options.add_argument('--start-maximized')
+# options.add_argument('--start-maximized')
+options.headless=True
+options.add_argument("window-size=1800x900")
 options.add_argument(user_agent)
+
 
 s = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=s, options=options)
+now = datetime.now()
 
 
-def prod_1(city):  # Сбор данных по продукту Лист/рулон г/к
+def prod_1(city_ru):  # Сбор данных по продукту Лист/рулон г/к
     prod1 = "Лист/рулон г/к"
     driver.find_element(By.XPATH, f"//a[text()='{prod1}']").click()  ### Лист/рулон г/к
-    # 4 6 8 10 12 16 18 20 28 32
     selected_prod1 = thickness_prod1
+    file = pd.DataFrame()
     for i in selected_prod1:
         time.sleep(3)
-        driver.find_element(By.XPATH, f"//a[text()='{str(i)}']").click()
+        driver.find_element(By.XPATH, f"//div/a[text()='{str(i)}']").click()
         page_source = driver.page_source
         soup = bs4.BeautifulSoup(page_source, "lxml")
         table = soup.select_one(".tablesorter")
         data = pd.read_html(str(table))
         data = data[0]
-        data.to_excel(
-            rf'C:\Users\Админ\Documents\PyProjects\Cost_monitoring\chromedriver\{city}\Лист_рулон_гк\prod1_thickness_{i}.xlsx',
-            index=False)
-        # print(f'prod1_thickness_{i}.xlsx -- ready')
+        data["Дата выгрузки данных"] = now.strftime("%d.%m.%Y")
+        data['Город']=city_ru
+        data["Продукт"] = prod1
+        data["Толщина"] = i
+        file=pd.concat([file,data])
+    file = file.drop(columns='Поставщик.1', axis=1)
     time.sleep(2)
+    return file
 
 
 def main_prod_1():
     driver.get(url)
-    cities = {"Moscow": "Москва", 'Saint Petersburg': 'Санкт-Петербург', 'Yekaterinburg': 'Екатеринбург',
-              'Nizhny Novgorod': 'Нижний Новгород', 'Novosibirsk': "Новосибирск", "Rostov-on-Don": "Ростов-на-Дону"}
+    cities = cities_start
     start_time = time.time()
+    combined = pd.DataFrame()
     for i in tqdm(cities, desc=f"Выгрузка Лист_рулон_гк"):
         driver.find_element(By.XPATH, f"//div[text()='Изменить/выбрать несколько']").click()
         time.sleep(2)
         driver.find_element(By.XPATH, f"//span[text()='Сбросить все']").click()
         time.sleep(2)
-        driver.find_element(By.XPATH, f"//a[text()='{cities[i]}']").click()
+        driver.find_element(By.XPATH, f"//a[text()='{i}']").click()
         time.sleep(2)
-        prod_1(i)
-    print(f"Выгрузка Лист_рулон_гк занала {time.time() - start_time} секунд")
+        file=prod_1(i)
+        combined = pd.concat([combined, file])
+
+    combined.to_excel(rf"{path_to_project}/Excel/ЦМ_{now.strftime('%Y-%m-%d')}_01_Лист_рулон_гк.xlsx",index=False)
+
+    print()
+    print(f"Выгрузка Лист_рулон_гк заняла {int((time.time()-start_time)/60)} минут {int((time.time()-start_time)/60)} секунд")
 
 
 
