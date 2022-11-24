df73c41b26815a333c18742cca172e061d966ee9
Airatich <106591216+Airatich@users.noreply.github.com>
2022-11-24 09:35:06 +0300

    sec

--- prod4.py
+++ prod4.py
@@ -11,62 +11,71 @@ import re
 from selenium.webdriver.chrome.service import Service
 import pandas as pd
 from webdriver_manager.chrome import ChromeDriverManager
-from options import user_agent,thickness_prod4
+from options import user_agent,thickness_prod4,path_to_project,cities_start
 from tqdm.auto import tqdm
 import time
+from datetime import datetime
+import random
+
+
+
 url = "http://23met.ru/"
 options = webdriver.ChromeOptions()
-# options.add_argument('--headless')
-options.add_argument('--start-maximized')
-options.add_argument(user_agent)
+# options.add_argument('--start-maximized')
+options.headless=True
+options.add_argument("window-size=1800x900")
+options.add_argument('Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36')
+
 
 s = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=s, options=options)
 
+now = datetime.now()
 
-def prod_4(city):  # Сбор данных по продукту Лист/рулон окраш.
+def prod_4(city_ru):  # Сбор данных по продукту Лист/рулон окраш.
     time.sleep(2)
     prod4="Лист/рулон окраш."
     driver.find_element(By.XPATH,f"//a[text()='{prod4}']").click()  ### Лист/рулон окраш.
     time.sleep(2)
-    # 0, 4
-    # 0, 45
-    # 0, 5
-    # 0, 55
-    # 0, 7
-    # 1
-
     selected_prod1 =thickness_prod4
-
+    file = pd.DataFrame()
     for i in selected_prod1:
         time.sleep(3)
-
         driver.find_element(By.XPATH, f"//div/a[text()='{str(i)}']").click()
         page_source = driver.page_source
         soup = bs4.BeautifulSoup(page_source, "lxml")
         table = soup.select_one(".tablesorter")
         data = pd.read_html(str(table))
         data = data[0]
-        data.to_excel(
-            rf'C:\Users\Админ\Documents\PyProjects\Cost_monitoring\chromedriver\{city}\Лист_рулон_окраш\prod3_thickness_{i}.xlsx',
-            index=False)
-    driver.find_element(By.XPATH, "/html/body").click()
+        data["Дата выгрузки данных"] = now.strftime("%d.%m.%Y")
+        data['Город'] = city_ru
+        data["Продукт"] = prod4
+        data["Толщина"] = i
+        file = pd.concat([file, data])
+    file = file.drop(columns='Поставщик.1', axis=1)
+    time.sleep(2)
+    return file
 
 
 
 def main_prod_4():
     driver.get(url)
-    cities = {"Moscow": "Москва", 'Saint Petersburg': 'Санкт-Петербург', 'Yekaterinburg': 'Екатеринбург',
-              'Nizhny Novgorod': 'Нижний Новгород', 'Novosibirsk': "Новосибирск", "Rostov-on-Don": "Ростов-на-Дону"}
+    cities = cities_start
     start_time = time.time()
+    combined = pd.DataFrame()
     for i in tqdm(cities, desc=f"Лист/рулон окраш."):
         driver.find_element(By.XPATH, f"//div[text()='Изменить/выбрать несколько']").click()
         time.sleep(2)
         driver.find_element(By.XPATH, f"//span[text()='Сбросить все']").click()
         time.sleep(2)
-        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH,
-                                                                    f"//a[text()='{cities[i]}' and @class='citychooser-city-link']"))).click()
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH,f"//a[text()='{i}' and @class='citychooser-city-link']"))).click()
         time.sleep(2)
-        prod_4(i)
-    print(f"Выгрузка Лист/рулон окраш. занала {time.time() - start_time} секунд")
+        file = prod_4(i)
+        combined = pd.concat([combined, file])
+
+    combined.to_excel(rf"{path_to_project}/Excel/ЦМ_{now.strftime('%Y-%m-%d')}_04_Лист_рулон_пп.xlsx", index=False)
+    print()
+    print(f"Выгрузка Лист/рулон окраш. занала {int((time.time()-start_time)/60)} минут {int((time.time()-start_time)/60)} секунд")
+
+
 
